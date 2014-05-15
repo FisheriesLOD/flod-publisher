e30b26a6abfffbbec90174c79794448526e117ba
flowerside <flowerside@gmail.com>
2014-05-15 18:47:26 +0200

    Adding examples of scheduled tasks management.

--- flod-publisher/src/main/java/org/fao/fi/flod/publisher/Publisher.java
+++ flod-publisher/src/main/java/org/fao/fi/flod/publisher/Publisher.java
@@ -5,14 +5,15 @@
  */
 package org.fao.fi.flod.publisher;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+
 import org.fao.fi.flod.publisher.store.task.PublicationTask;
 import org.fao.fi.flod.publisher.store.task.TaskStore;
 import org.slf4j.Logger;
@@ -23,28 +24,64 @@ import org.slf4j.LoggerFactory;
  * @author Claudio Baldassarre <c.baldassarre@me.com>
  */
 public class Publisher {
-
     private static Logger log = LoggerFactory.getLogger(Publisher.class);
-    private HashMap<PublicationTask, Boolean> taskRegistry = new HashMap();
 
+    final private Map<PublicationTask, Boolean> taskRegistry = Collections.synchronizedMap(new HashMap<PublicationTask, Boolean>());
+    final private ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(2);
+
+    final static int SCHEDULED_FREQUENCY = 2 * 1000;
+    
     public static void main(String[] args) {
+    	final Publisher $this = new Publisher();
+    	
+    	$this.SCHEDULER.scheduleAtFixedRate(new Runnable() {
+    		@Override
+    		public void run() {
+    			long id = System.currentTimeMillis();
+    			
+//    			$this.retrieveTasks();
+    			System.out.println("RETRIEVE TASKS : BEGIN #" + id);
 
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(5);
+    			long sleepTime = Math.round(Math.random() * 2.5 * SCHEDULED_FREQUENCY);
+    			try {
+    				System.out.println("Retriever going to sleep for " + sleepTime + " mSec... 'nite!");
 
-        ScheduledFuture scheduledFuture = scheduledExecutorService.schedule(new Callable() {
-            public Object call() throws Exception {
-                System.out.println("Executed!");
-                return "Called!";
-            }
-        },
-                5,
-                TimeUnit.SECONDS);
-
-//        System.out.println("result = " + scheduledFuture.get());
-//        scheduledExecutorService.shutdown();
+    				//Simulates long computation times...
+    				Thread.sleep(sleepTime);
+    			} catch(InterruptedException Ie) {
+    				throw new RuntimeException(Ie);
+    			}
+    			
+    			System.out.println("RETRIEVE TASKS : END #" + id);
+    		}
+    	}, 0, SCHEDULED_FREQUENCY, TimeUnit.MILLISECONDS);
+    	
+    	$this.SCHEDULER.scheduleAtFixedRate(new Runnable() {
+    		@Override
+    		public void run() {
+    			long id = System.currentTimeMillis();
+    			
+//    			$this.execute();
+    			System.out.println("EXECUTE : BEGIN #" + id);
+    			
+    			long sleepTime = Math.round(Math.random() * 2.5 * SCHEDULED_FREQUENCY);
+    			
+    			try {
+    				System.out.println("Executor going to sleep for " + sleepTime + " mSec... 'nite!");
+    				
+    				//Simulates long computation times...
+    				Thread.sleep(sleepTime);
+    			} catch(InterruptedException Ie) {
+    				throw new RuntimeException(Ie);
+    			}
+    			
+    			System.out.println("EXECUTE : END #" + id);
+    		}    		
+    	}, SCHEDULED_FREQUENCY / 2, SCHEDULED_FREQUENCY, TimeUnit.MILLISECONDS);
     }
 
-    private void retrieveTasks() {
+    @SuppressWarnings("unused")
+	synchronized private void retrieveTasks() {
         List<PublicationTask> tasks = TaskStore.getInstance().listTasks();
         for (PublicationTask pt : tasks) {
             boolean executed = this.taskRegistry.containsKey(pt);
@@ -63,7 +100,8 @@ public class Publisher {
         }
     }
 
-    private void execute() {
+    @SuppressWarnings("unused")
+	synchronized private void execute() {
         Set<PublicationTask> tasks = this.taskRegistry.keySet();
         for (PublicationTask pt : tasks) {
             try {
